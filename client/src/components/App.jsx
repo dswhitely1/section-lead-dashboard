da103472aeaf2bdcc7fea69dc6e0340b9a33c26a
Donald Whitely <dswhitely1@gmail.com>
2019-10-12 15:21:20 -0400

    Brought Actions into Context

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ActionsProvider } from '../contexts/ActionsContext';
+import { useAuthActions } from '../store/auth/useAuthActions';
 
 function App() {
-  return <div>Hello World</div>;
+  const authActions = useAuthActions();
+  return (
+    <ActionsProvider value={{ authActions }}>
+      <h1>Hello World</h1>
+    </ActionsProvider>
+  );
 }
 
 export default App;
