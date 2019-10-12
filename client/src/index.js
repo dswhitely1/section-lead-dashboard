6854a8115a70ed836a8766ce581084c3b786453a
Donald Whitely <dswhitely1@gmail.com>
2019-10-12 14:25:34 -0400

    Build My Own React App

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const rootElement = document.getElementById('section-lead-dashboard');
+
+render(<div>Hello World</div>, rootElement);
