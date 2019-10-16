f05fc68669b87d0e2299e990f317e83a0950bb59
Donald Whitely <dswhitely1@gmail.com>
2019-10-16 17:24:14 -0400

    Added Grid to Sprint Dashboard

--- client/src/components/sprints/SprintsDashboard.jsx
+++ client/src/components/sprints/SprintsDashboard.jsx
@@ -5,6 +5,7 @@ import SprintNavigation from './SprintNavigation';
 import SprintCard from './SprintCard';
 import Container  from '@material-ui/core/Container';
 import {makeStyles} from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme=>({
   cards: {
@@ -18,9 +19,11 @@ const classes=useStyles();
     <>
       <SprintNavigation />
       <Container className={classes.cards}>
+        <Grid container spacing={3}>
       {sprints.map(sprint => (
         <SprintCard key={sprint.id} sprint={sprint} />
       ))}
+        </Grid>
       </Container>
     </>
   );
